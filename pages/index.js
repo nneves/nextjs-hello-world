79b067513ac240fe50628e9d16c382a51308614d
Nelson Neves <nelson.s.neves@gmail.com>
2021-01-19 17:42:59 +0000

    some additiona changes to the master

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to the amazing world of <a href="https://nextjs.org">Next.js!</a>
+        🔍 Welcome to the amazing world of <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
